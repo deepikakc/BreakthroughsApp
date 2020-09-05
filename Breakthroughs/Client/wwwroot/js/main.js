57d1331342fa7ec3b7795e3897e8ceeb8f03504e
Mihajlo Biševac <kajler11@gmail.com>
2020-09-05 16:45:13 +0200

    Scrollbar displayed on mobile

--- Breakthroughs/Client/wwwroot/js/main.js
+++ Breakthroughs/Client/wwwroot/js/main.js
@@ -1,7 +1,7 @@
 ﻿function nSelected(selected, previous) {
     var selectElem = document.getElementById(selected)
     var deselectElem = document.getElementById(previous)
-    
+
     deselectElem.className = "list"
     selectElem.className = "list list-selected"
 }
